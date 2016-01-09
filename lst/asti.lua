426ac1bdc47689310e290c1a222d58c9b7bfcd99
Dmitry Ponyatov <dponyatov@gmail.com>
2016-01-09 15:20:52 +0400

    (v) habr57 tutor
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ lst/asti.lua
@@ -0,0 +1,5 @@
+if (sim_call_type==sim_childscriptcall_initialization) then 
+	asti=simGetObjectHandle("Asti")
+	lFoot=simGetObjectHandle("leftFootTarget")
+	rFoot=simGetObjectHandle("rightFootTarget")
+...
